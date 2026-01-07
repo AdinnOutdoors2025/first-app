52dc8a7adc16dd2c93525f1a910525693d03e7cb
AdinnOutdoor <reactdeveloper@adinn.co.in>
2026-01-07 18:43:37 +0530

    baserURL changed

--- src/Adminpanel/BASE_URL.js
+++ src/Adminpanel/BASE_URL.js
@@ -1,3 +1,3 @@
-// export const baseUrl = "https://backend-bq11.onrender.com";
-export const baseUrl = "http://localhost:3001";
+export const baseUrl = "https://backend-bq11.onrender.com";
+// export const baseUrl = "http://localhost:3001";
 // export const baseUrl = "http://192.168.0.159:3001";
