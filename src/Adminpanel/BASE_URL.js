04106ccd051e275d1d2d59e2ef1f401341822cfd
Karthika <reactdeveloper@adinn.co.in>
2026-02-16 12:49:13 +0530

    baseUrlUpdated1

--- src/Adminpanel/BASE_URL.js
+++ src/Adminpanel/BASE_URL.js
@@ -1,10 +1,10 @@
-// export const baseUrl = "https://backend-bq11.onrender.com";
+export const baseUrl = "https://backend-bq11.onrender.com";
 // export const baseUrl = "http://localhost:3001";
 // export const baseUrl = "http://192.168.0.159:3001";
 export const gstPercentage = 18 //18% gst on each order of the product
 export const whatsAppNumber = '917092558277'
 // export const baseUrl = "https://backend-git-main-adinnoutdoors-projects.vercel.app";
-export const baseUrl = "https://backend-plum-two-80.vercel.app/";
+// export const baseUrl = "https://backend-plum-two-80.vercel.app/";
 
 
 
