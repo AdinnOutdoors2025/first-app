9f13a1b962029b5270ba99411b89adb721a4d85d
AdinnOutdoors2025 <adinndigitalservices@gmail.com>
2026-02-21 11:23:43 +0530

    localportConnected

--- src/Adminpanel/BASE_URL.js
+++ src/Adminpanel/BASE_URL.js
@@ -1,6 +1,6 @@
 // export const baseUrl = "https://backend-bq11.onrender.com";
-// export const baseUrl = "http://localhost:3001";
-export const baseUrl = "http://142.93.213.3:3001";
+export const baseUrl = "http://localhost:3001";
+// export const baseUrl = "http://142.93.213.3:3001";
 // export const baseUrl = "http://192.168.0.159:3001";
 export const gstPercentage = 18 //18% gst on each order of the product
 export const whatsAppNumber = '917092558277'
