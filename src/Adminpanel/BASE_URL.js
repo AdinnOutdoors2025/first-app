0843c3a6bd1fd64fc7140757c7da4f2421773217
AdinnOutdoors2025 <adinndigitalservices@gmail.com>
2026-04-11 15:30:30 +0530

    baseUrlChanged

--- src/Adminpanel/BASE_URL.js
+++ src/Adminpanel/BASE_URL.js
@@ -1,6 +1,6 @@
-export const baseUrl = "http://localhost:3001"; // Local platform Backend 
+// export const baseUrl = "http://localhost:3001"; // Local platform Backend 
 // export const baseUrl = "http://192.168.2.134:3001"; // Local platform Backend 
-// export const baseUrl = "/api"; // Digital Ocean platform Backend URL
+export const baseUrl = "/api"; // Digital Ocean platform Backend URL
 export const gstPercentage = 18 //18% gst on each order of the product
 export const whatsAppNumber = '917092558277'
 
